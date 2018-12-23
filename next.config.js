9f3adbb776a3148b4b8ae89757e179482224068f
Evgeniy Bulgakov <zoom81@yandex.ru>
2018-12-23 14:04:02 +0400

    Fixing build process

--- next.config.js
+++ next.config.js
@@ -1,21 +1,34 @@
-// next.config.js
-const withTypescript = require('@zeit/next-typescript');
-const withSass = require('@zeit/next-sass');
+const { PHASE_PRODUCTION_SERVER } =
+  process.env.NODE_ENV === 'development'
+    ? {} // We're never in "production server" phase when in development mode
+    : !process.env.NOW_REGION
+    ? require('next/constants') // Get values from `next` package when building locally
+    : require('next-server/constants'); // Get values from `next-server` package when building on now v2
 
-const nextConfig = {
-  cssModules: true,
-  cssLoaderOptions: {
-    importLoaders: 1,
-    localIdentName: '[name]-[local]___[hash:base64:5]',
-  },
-  webpack(config, options) {
-    config.module.rules.push({
-      test: /\.svg/,
-      use: ['@svgr/webpack'],
-    });
+module.exports = (phase, { defaultConfig }) => {
+  if (phase === PHASE_PRODUCTION_SERVER) {
+    // Config used to run in production.
+    return {};
+  }
 
-    return config;
-  },
+  const withTypescript = require('@zeit/next-typescript');
+  const withSass = require('@zeit/next-sass');
+
+  const nextConfig = {
+    cssModules: true,
+    cssLoaderOptions: {
+      importLoaders: 1,
+      localIdentName: '[name]-[local]___[hash:base64:5]',
+    },
+    webpack(config) {
+      config.module.rules.push({
+        test: /\.svg/,
+        use: ['@svgr/webpack'],
+      });
+
+      return config;
+    },
+  };
+
+  return withTypescript(withSass(nextConfig));
 };
-
-module.exports = withTypescript(withSass(nextConfig));
