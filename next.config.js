cd4ba875412e176f7e4fe87d4e6d75280813a3de
Evgeniy Bulgakov <zoom81@yandex.ru>
2018-12-23 04:48:05 +0400

    Remove unused deps

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 // next.config.js
 const withTypescript = require('@zeit/next-typescript');
 const withSass = require('@zeit/next-sass');
-const withImages = require('next-images');
 
 const nextConfig = {
   cssModules: true,
