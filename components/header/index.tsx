9638f0f530aa802b45d6bbb6df498259edffcdd3
Evgeniy Bulgakov <zoom81@yandex.ru>
2018-12-23 15:04:17 +0400

    Add some responce

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -7,10 +7,28 @@ import Twitter from './twitter.svg';
 import Facebook from './facebook.svg';
 
 export default class extends React.Component {
+  state = {
+    scroll: false,
+  };
+  scrollHeader = () => {
+    const offsetTop = window.pageYOffset ? window.pageYOffset : document.documentElement!.scrollTop;
+
+    this.setState({
+      scroll: offsetTop > 0,
+    });
+  };
+  componentDidMount() {
+    this.scrollHeader();
+    window.addEventListener('scroll', this.scrollHeader);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.scrollHeader);
+  }
   render() {
     return (
       <header className={styles.static}>
-        <div className={styles.runner}>
+        <div className={this.state.scroll ? styles.invert : styles.normal}>
           <div className={styles.container}>
             <Link href="/">
               <a className={styles.logo}>
@@ -20,10 +38,14 @@ export default class extends React.Component {
             <Menu />
 
             <Link href="/">
-              <a className={styles.social}><Twitter width={18} height={18} /></a>
+              <a className={styles.social}>
+                <Twitter width={18} height={18} />
+              </a>
             </Link>
             <Link href="/">
-              <a className={styles.social}><Facebook width={10} height={16} /></a>
+              <a className={styles.social}>
+                <Facebook width={10} height={16} />
+              </a>
             </Link>
             <Link href="/">
               <a className={styles.language}>ru</a>
